b556972f8abe5918d8d725522b3df6e366c89139
wolf09 <jaybhayevaibhav162@gmail.com>
2025-12-05 17:19:55 +0530

    ok

--- dsa/move_zero.py
+++ dsa/move_zero.py
@@ -4,12 +4,12 @@ class solution:
     def sol(self,nums):
         w = 0
         for i in range(len(nums)):
-            if nums[i] != 0:
+            if nums[i] != 4:
                 nums[i], nums[w] = nums[w], nums[i]
                 w += 1
         return nums
 
 
-nums = [2,335,0,56,43,0]
+nums = [2,4,4,335,0,56,43,0]
 d = solution()
 print(d.sol(nums))
