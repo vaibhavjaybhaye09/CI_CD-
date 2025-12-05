ed854d28071a2f99e57496ea91acd372279f39b5
wolf09 <jaybhayevaibhav162@gmail.com>
2025-12-05 18:20:32 +0530

    sone

---
+++ dsa/list_merge.py
@@ -0,0 +1,18 @@
+
+
+
+class solution:
+    def sol(self,nums,num):
+        merge = nums + num
+        print(list(set(merge)))
+        
+       
+
+        
+     
+
+
+nums = [1, 2, 3, 4, 5]
+num = [1, 3, 11, 22, 33, 44, 66]    
+d = solution()
+print(d.sol(nums,num))
