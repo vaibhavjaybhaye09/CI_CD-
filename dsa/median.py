96dfee1c2d8a4d4b945f4de2548afcfc7565bdb1
wolf09 <jaybhayevaibhav162@gmail.com>
2025-12-07 01:37:11 +0530

    true

--- dsa/median.py
+++ dsa/median.py
@@ -1,17 +1,13 @@
 class Solution:
     def sol(self,nums1,nums2):
-        merge_list = sorted(nums1+nums2)
-        print(merge_list)
-        length = len(merge_list)  
+        arr = nums1 + nums2
+        arr.sort()
+        m = len(arr)
 
-        if length % 2 == 0:
-           mid1 = merge_list[(length // 2) - 1]
-           mid2 = merge_list[(length // 2)]
-           sum_ofmedian  = mid1 + mid2
-           return f'{sum_ofmedian / 2:.4f}'
-        else:          
-            mid2 = merge_list[(length // 2)]
-            return f"{mid2 / 2:.4f}"
+        if m % 2 == 0:
+            return (arr[m // 2 -1] + arr[m // 2]) / 2
+        else:
+            return arr[m // 2]
 
     
 
