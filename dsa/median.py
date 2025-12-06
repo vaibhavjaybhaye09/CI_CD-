19773a1720f2f4c3591c293b7223f496f456f52a
wolf09 <jaybhayevaibhav162@gmail.com>
2025-12-07 01:20:31 +0530

    done this prooblem

--- dsa/median.py
+++ dsa/median.py
@@ -1,3 +1,23 @@
-num  = [1, 2, 3, 4, 5, 6]
+class Solution:
+    def sol(self,nums1,nums2):
+        merge_list = sorted(nums1+nums2)
+        print(merge_list)
+        length = len(merge_list)  
 
-k= 0
+        if length % 2 == 0:
+           mid1 = merge_list[(length // 2) - 1]
+           mid2 = merge_list[(length // 2)]
+           sum_ofmedian  = mid1 + mid2
+           return f'{sum_ofmedian / 2:.4f}'
+        else:          
+            mid2 = merge_list[(length // 2)]
+            return f"{mid2 / 2:.4f}"
+
+    
+
+nums1 = [1, 2, 3]
+nums2 = [6, 7, 9]
+ans = Solution()
+print(ans.sol(nums1,nums2))
+
+
