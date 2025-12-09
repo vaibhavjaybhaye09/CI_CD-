703662ef17ad8392d9ce44fa9bbd6da44db3f40a
wolf09 <jaybhayevaibhav162@gmail.com>
2025-12-09 19:14:09 +0530

    frequncy count

--- dsa/hasmao/count_frquncy_inList.py
+++ dsa/hasmao/count_frquncy_inList.py
@@ -1,15 +1,13 @@
 num = [ 1, 2, 3, 4, 5, 5, 3, 34, 4]
 
 n = num
-frequency_map = dict()
+freq_map = dict()
 
-for i in range(0 ,len(n)):
-    if num[i] in frequency_map:
-        frequency_map[num[i]] += 1
+for i in range(0, len(n)):
+    if num[i] in freq_map:
+        freq_map[num[i]] += 1
     else:
-        frequency_map[num[i]] = 1
+        freq_map[num[i]] = 1
 
 
-print(frequency_map[num[5]])
-
-        
+print(freq_map[num[3]])
