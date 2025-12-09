f42ab0fd724d953cad0c15e4928708715fb51ace
wolf09 <jaybhayevaibhav162@gmail.com>
2025-12-09 19:26:48 +0530

    same noumber count

--- dsa/count.ALl_dupicate.py
+++ dsa/count.ALl_dupicate.py
@@ -1,7 +1,10 @@
-num = [1, 3, 2, 5, 5, 5]
+num = [1, 3, 1, 4, 4, 2, 5, 5, 5]
 c = 0
 for i in range(0,len(num) - 1):
         for j in range(i + 1, len(num)):
                 if num[i] == num[j - 2]:
+                        c +=1
+        
+
+print(c)
                         
-            
---
+++ dsa/count_singleElement_dupplicate.py
@@ -0,0 +1,9 @@
+num = [1, 1, 3, 4, 5, 5]
+c = 0
+sum = 5
+for i in range(0,len(num)):
+    if num[i] == sum:
+        c += 1
+
+print(c)
+
