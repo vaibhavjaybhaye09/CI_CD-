7797bcc681f0c2709c6d626c7f80b359d02f8656
wolf09 <jaybhayevaibhav162@gmail.com>
2025-12-17 21:39:04 +0530

    ok

--- example.py
+++ example.py
@@ -1,13 +1,5 @@
+vaibhav = """Hello,my,name,vaibhav"""
 
-# num = 101
-# while num >= 91:
-#     num = num - 1
-#     print(num)
+print(len(vaibhav.split(',')))
 
-num = 0
-while num <= 45:
-    num = num + 5
-    print(num)
-
-
-
+print(isinstance(vaibhav, str))
