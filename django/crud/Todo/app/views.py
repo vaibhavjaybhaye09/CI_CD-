e24f10ba0032c83b85f40b7f5e453431f6b6b050
wolf09 <jaybhayevaibhav162@gmail.com>
2026-06-03 01:12:10 +0530

    some change

--- django/crud/Todo/app/views.py
+++ django/crud/Todo/app/views.py
@@ -6,7 +6,5 @@ from .serializers import TodoSerializer
 class TodoVIewset(viewset.Modelviewset):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-    renderer_classes = [renderers.JSONRenderer]
-    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
     required_fields = ['todo']
     template_name = 'app/home.html'
